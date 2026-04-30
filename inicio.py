26f5afe652ddcf01416e9f21a709093daa90c1ce
alexherrerayanez-bit <alex.herrera.yanez@gmail.com>
2026-04-30 15:52:59 -0400

    ejercicio clases

--- inicio.py
+++ inicio.py
@@ -27,21 +27,126 @@
 
 
 
-def VocalesConsonantes():
-    nombre=input("Ingrese su nombre: ")
-    vocales=0
-    cons=0
-    for i in nombre:
-        print(i)
-        # vocales=vocales+1
-        if i in "aeiou":
-            vocales+=1
-        elif i==" ":
-            print()
-        else:
-            cons+=1
-    print(f"La cant de vocales es {vocales}")
-    print(f"La cant de consonantes es {cons}")
-    VocalesConsonantes
+# def VocalesConsonantes():
+#     nombre=input("Ingrese su nombre: ")
+#     vocales=0
+#     cons=0
+#     for i in nombre:
+#         print(i)
+#         # vocales=vocales+1
+#         if i in "aeiou":
+#             vocales+=1
+#         elif i==" ":
+#             print()
+#         else:
+#             cons+=1
+#     print(f"La cant de vocales es {vocales}")
+#     print(f"La cant de consonantes es {cons}")
+#     VocalesConsonantes
+
+
+# import random
+
+# num=random.randint(1,10)
+# print(num)
+
+
+# for i in range(num):
+#     print("hola alex")
+
+
+#     dado=random.randint(1,6)
+#     print("el dado salio", dado)
+
+
+
+    #crear una tabla de multiplicar
+    #de 1 a 24, mostrar la tabla
+
+# tirar 2 dados, y si sale par se va a la carcel, sino avanza
+
+
+#TABLA DE MULTIPLICAR HASTA EL 24
+
+# num=int(input("Ingrese un numero: "))
+
+# for i in range(1,24):
+#     print(num, "x" , i+1 , "=", num*(i+1))
+#     print(i)
+
+# for i in range(1,24):
+#     print(num, "x" , i , "=", num*i)
+
+
+
+#TIRAR 2 DADOS
+
+
+# import random
+    
+# dado1=random.randint(1,6)
+# dado2=random.randint(1,6)
+# print("el dado salio 1", dado1)
+# print("el dado salio 2", dado2)
+
+# if dado1==dado2:
+#     print("vayase alv")
+# else:
+#     print("siga avanzando")
+
+
+
+
+
+
+
+#3 JUGADORES DE GOLF
+
+
+# import random
+
+# jugador1=random.randint(60,190)
+# jugador2=random.randint(60,190)
+# jugador3=random.randint(60,190)
+# print("el jugador 1 llego hasta la distancia", jugador1)
+# print("el jugador 2 llego hasta la distancia", jugador2)
+# print("el jugador 3 llego hasta la distancia", jugador3)
+
+# if  jugador1> jugador2 and jugador1 > jugador3:
+#     print("el jugador 1 llego mas lejos")
+
+# elif jugador2>jugador1 and jugador2 > jugador3:
+#     print("el jugador 2 llego mas lejos")
+
+# else:
+#     print("el jugador 3 llego mas lejos")
+
+
+
+# import time
+# import random
+
+
+# time.sleep(1)
+
+# turno=1
+
+# if turno%2==0:
+#   print("turno uno")
+# else:
+#   print("turno dos")
+
+
+# vidapeleador1=100
+# peleador1=random.randint(7,18)
+# vidapeleador2=100
+# peleador2=random.randint(7,18)
+# print("el peleador 1 hizo daño", peleador1)
+# print("el peleador 2 hizo daño", peleador2)
+
+# if int<100:
+  
+
+  
 
 
