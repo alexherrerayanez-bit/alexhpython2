d3e9e1992e1e049be78d245607cf0c3996a6ddf0
alexherrerayanez-bit <alex.herrera.yanez@gmail.com>
2026-05-07 15:44:26 -0400

    commit

--- practiva.py
+++ practiva.py
@@ -6,19 +6,62 @@
    # print("el numero de letras de su nombre es:", n_letras)
   
 
-print("ingrese su nombre")
-name=input()
-vocales=0
-consonantes=0
-for i in name:
-    print(i)
-    if i in "aeiouAEIOU":
-        vocales=vocales+1
-    elif i=="":
-        ""
-    else:
-         consonantes=consonantes+1
-        print("la cantidad de vocales son {vocales}")
+
+
+# print("ingrese su nombre")
+# name=input()
+# vocales=0
+# consonantes=0
+# for i in name:
+#     print(i)
+#     if i in "aeiouAEIOU":
+#         vocales=vocales+1
+#     elif i=="":
+#         ""
+#     else:
+#          consonantes=consonantes+1
+#         print("la cantidad de vocales son {vocales}")
     
-        print("el numero de consonantes son {consonantes}")
+#         print("el numero de consonantes son {consonantes}")
+
+
+import random
+
+# num=random.randint(1,9)
+
+# while abs(-3)!=num:
+#    print(num)
+#    time.sleep(1)
+
+#    num=random.randint(1,9)
+
+# n1=int(input("ingrese el valor del limite inferior"))
+# n2=int(input("ingrese el valor del limite superior"))
+# while n2 <= n1:
+#     print("el limite superior no es mayor que el inferior")
+#     n2 = int(input("ingrese el valor del limite superior: "))
+# print("parametros correctos")
+# num=random.randint(n1,n2)
+# print(num)
+
+
+# plancha=0
+# lata=0
+# peso= random.randint(800,3000)
+# peces= random.randint(10,20)
+# print("salio un total de peces de:", peces)
+# print("el total de peso de peces es:", peso)
+# for i in range (peces):
+#     peso = random.randint(500, 3000)
+#     if peso<=800:
+#        lata += 1
+#     elif peso >= 801 and peso <= 3000:
+#         plancha += 1
+#     else: 
+#         print("peso invalido")
+
+# print(f"el total de peces en latas son {lata}")
+# print(f"el total de peces en plancha son {plancha}")
+
+
 
